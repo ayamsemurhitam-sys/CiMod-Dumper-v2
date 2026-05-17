d2ae03051bbc84f70dc6a69333b7a802c62d475a
ayamsemurhitam-sys <ayamsemurhitam@gmail.com>
2026-05-18 05:40:46 +0700

    Add files via upload

---
+++ module/src/main/cpp/game.h
@@ -0,0 +1,10 @@
+//
+// Created by CiMOD on 2026/5/17.
+//
+
+#ifndef ZYGISK_IL2CPPDUMPER_GAME_H
+#define ZYGISK_IL2CPPDUMPER_GAME_H
+
+#define GamePackageName "com.mobile.legends"
+
+#endif //ZYGISK_IL2CPPDUMPER_GAME_H
